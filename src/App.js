6b84f5fa9c7dbfddc5a52f3e96ffb4c5bae4914e
Theebaseharan <theebakms2004@gmail.com>
2024-07-08 08:01:58 +0530

    fix: edited content in app.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
         <p>
           Theebaseharan M S.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Networks and DevOps Engineer
+        </p>
+        
       </header>
     </div>
   );
